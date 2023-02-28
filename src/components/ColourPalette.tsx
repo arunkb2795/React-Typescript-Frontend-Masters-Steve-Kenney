53240094474d7322b3440891a80a69733e0c1b0d
arunkb2795 <70054643+arunkb2795@users.noreply.github.com>
2023-02-28 13:11:47 +0530

    color picker update

--- src/components/ColourPalette.tsx
+++ src/components/ColourPalette.tsx
@@ -1,7 +1,24 @@
+import { useReducer } from 'react';
+import colorReducer, { initialState } from '../lib/color-reducer';
 export default function ColorPalette() {
+  const [state, dispatch] = useReducer(colorReducer, initialState);
   return (
     <div>
       <p>Color Palette</p>
+      <input
+        type="color"
+        value={state.hexColor}
+        onChange={(e) =>
+          dispatch({
+            type: 'update-hex-color',
+            payload: { hexColor: e.target.value },
+          })
+        }
+      />
+      <div
+        className={`w-20 h-20`}
+        style={{ backgroundColor: state.hexColor }}
+      ></div>
     </div>
   );
 }
