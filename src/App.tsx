5ce8587d4719d68fea98974370ed4290811f298f
arunkb2795 <70054643+arunkb2795@users.noreply.github.com>
2023-02-28 11:19:16 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import NameBadge from './components/NameBadge';
-import Controls from './components/Controls';
-import Application from './components/Application';
-import Counter from './components/Counter';
-import FetchAPI from './components/FetchAPI';
-import UseReducerCounter from './components/UseReducerCounter';
+import ColorPalette from './components/ColourPalette';
 
 function App() {
   const [value, setValue] = useState('');
@@ -14,16 +9,7 @@ function App() {
   };
   return (
     <>
-      <NameBadge name={value} greeting={'Welcome'} />
-      <Controls
-        name={value}
-        onChange={(event) => setValue(event.target.value)}
-        onSubmit={onSubmit}
-      />
-      <Application />
-      <Counter />
-      <UseReducerCounter />
-      <FetchAPI />
+      <ColorPalette />
     </>
   );
 }
---
+++ src/components/ColourPalette.tsx
@@ -0,0 +1,7 @@
+export default function ColorPalette() {
+  return (
+    <div>
+      <p>Color Palette</p>
+    </div>
+  );
+}
